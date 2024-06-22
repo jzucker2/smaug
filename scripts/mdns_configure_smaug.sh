8f92ec7d83080fea2a859a84ae01dfb7bbbd5be6
Jordan Zucker <jordan.zucker@gmail.com>
2024-06-22 12:56:46 -0700

    Cleaning up some more

--- scripts/mdns_configure_smaug.sh
+++ scripts/mdns_configure_smaug.sh
@@ -1,17 +1,15 @@
 #!/bin/sh
 
-MDNS_NAME="gerlack"
+MDNS_NAME="smaug"
 
 SCRIPTS_DIR=scripts
 PI_SCRIPTS_DIR=${SCRIPTS_DIR}/pi
 
 STEPS=2
-echo "Set up gerlack mDNS in ${STEPS} steps!"
+echo "Set up smaug mDNS in ${STEPS} steps!"
 
-echo "[1/${STEPS}] update git branch"
-git fetch --all
+echo "[1/${STEPS}] echo git status"
 git status
-git pull
 
 echo "[2/${STEPS}] set up mDNS with MDNS_NAME: ${MDNS_NAME} (this invokes a reboot)"
 sh ${PI_SCRIPTS_DIR}/mdns_configure.sh ${MDNS_NAME}
